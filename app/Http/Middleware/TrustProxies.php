214e5f64b151f04bb71bcb6a0124b18ba80eb9fc
ijonk7 <jookeo.rizal@gmail.com>
2021-03-07 21:10:26 +0700

    add trusted proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = ['10.0.1.46'];
 
     /**
      * The headers that should be used to detect proxies.
